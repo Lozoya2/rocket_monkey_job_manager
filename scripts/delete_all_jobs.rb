da93e538c3996e900b380c3c895377cc40da752b
Robert Lozoya <robert.lozoya@rightscale.com>
2016-01-14 12:03:50 -0800

    custom config for ss/cwf

--- scripts/delete_all_jobs.rb
+++ scripts/delete_all_jobs.rb
@@ -5,7 +5,7 @@ require 'byebug'
 # every result that is included in this list will be chained into a loop. 
 
 @client = JenkinsApi::Client.new(YAML.load_file(File.expand_path(
-  "../config/login.yml", __FILE__)))
+  "../../config/login.yml", __FILE__)))
 
 # To properly grab all associated Jenkins jobs for a particular app/shard, make 
 # sure to match the prefix pattern for example:
@@ -20,10 +20,18 @@ require 'byebug'
 # shard4_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD4')
 #
 # Which will give us all jobs tied into shard 3 and shard 4, respectively. 
-shard3_jobs = @client.job.list('')
-shard4_jobs = @client.job.list('')
+ss_api_shard3_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD3')
+ss_api_shard4_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD4')
 
-job_list = [shard3_jobs, shard4_jobs]
+# ss_ui_shard3_jobs  = @client.job.list('PROD_SS_UI_ssuislave1_ReleaseSHARD3')
+# ss_ui_shard4_jobs  = @client.job.list('PROD_SS_UI_ssuislave1_ReleaseSHARD4')
+
+cwf_shard3_jobs    = @client.job.list('PROD_CWF_cwfslave1_ReleaseSHARD3')
+cwf_shard4_jobs    = @client.job.list('PROD_CWF_cwfslave1_ReleaseSHARD4')
+
+job_list = [
+  ss_api_shard3_jobs, ss_api_shard4_jobs, cwf_shard3_jobs, cwf_shard4_jobs
+]
 
 job_list.each{|s|
   s.each{|x| @client.job.delete(x)}
--- scripts/loop_jobs.rb
+++ scripts/loop_jobs.rb
@@ -5,7 +5,7 @@ require 'byebug'
 # every result that is included in this list will be chained into a loop. 
 
 @client = JenkinsApi::Client.new(YAML.load_file(File.expand_path(
-  "../config/login.yml", __FILE__)))
+  "../../config/login.yml", __FILE__)))
 
 # To properly grab all associated Jenkins jobs for a particular app/shard, make 
 # sure to match the prefix pattern for example:
@@ -20,10 +20,18 @@ require 'byebug'
 # shard4_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD4')
 #
 # Which will give us all jobs tied into shard 3 and shard 4, respectively. 
-shard3_jobs = @client.job.list('')
-shard4_jobs = @client.job.list('')
+ss_api_shard3_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD3')
+ss_api_shard4_jobs = @client.job.list('PROD_SS_API_ssapislave1_ReleaseSHARD4')
 
-job_list = [shard3_jobs, shard4_jobs]
+# ss_ui_shard3_jobs  = @client.job.list('PROD_SS_UI_ssuislave1_ReleaseSHARD3')
+# ss_ui_shard4_jobs  = @client.job.list('PROD_SS_UI_ssuislave1_ReleaseSHARD4')
+
+cwf_shard3_jobs    = @client.job.list('PROD_CWF_cwfslave1_ReleaseSHARD3')
+cwf_shard4_jobs    = @client.job.list('PROD_CWF_cwfslave1_ReleaseSHARD4')
+
+job_list = [
+  ss_api_shard3_jobs, ss_api_shard4_jobs, cwf_shard3_jobs, cwf_shard4_jobs
+]
 
 # If any jobs exist that should not be integrated into the main loop, then list 
 # them here, for example:
@@ -35,17 +43,30 @@ job_list = [shard3_jobs, shard4_jobs]
 # jobs_not_to_chain = [/dont_loop_me_functional_test/]
 # 
 # This will skip said tests on both shards so you can manage them manually
-jobs_not_to_chain = []
+ss_api_jobs_not_to_chain = [/.functional_CAT_test/]
 
-jobs_not_to_chain.each{|s|
-  shard3_jobs.delete_if{|t| s=~t}
-  shard4_jobs.delete_if{|t| s=~t}
+ss_api_jobs_not_to_chain.each{|s|
+  ss_api_shard3_jobs.delete_if{|t| s=~t}
+  ss_api_shard4_jobs.delete_if{|t| s=~t}
+}
+
+# ss_ui_jobs_not_to_chain = []
+
+# ss_ui_jobs_not_to_chain.each{|s|
+#   ss_ui_shard3_jobs.delete_if{|t| s=~t}
+#   ss_ui_shard4_jobs.delete_if{|t| s=~t}
+# }
+
+cwf_jobs_not_to_chain = []
+
+cwf_jobs_not_to_chain.each{|s|
+  cwf_shard3_jobs.delete_if{|t| s=~t}
+  cwf_shard4_jobs.delete_if{|t| s=~t}
 }
 
 job_list.each{|s|
-  # This only chains jobs sequentially, does not create the closing loop
   @client.job.chain(s, 'success', ['all'])
-  # Loop is created here
+  # Jobs are chained now we create the loop
   @client.job.update_freestyle({
     name: s.last, child_projects: s.first, child_threshold: 'success'
   })
